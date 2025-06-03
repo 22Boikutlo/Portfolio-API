5778536b1af8dcf72b103400bcd8b4772ef7b8b9
22Boikutlo <boikutlos@gmail.com>
2025-06-04 00:43:40 +0200

    added picture field
    Add Get method and update Put method in OwnerController
    
    - Implemented a new Get method to retrieve owner details by ID from the database.
    - Fixed a typo in the Put method's SQL query and added OwnerId parameter for updates.
    - Commented out the SaveFile method for potential future use.

--- Controllers/OwnerController.cs
+++ Controllers/OwnerController.cs
@@ -17,6 +17,34 @@ namespace PortfolioAPI.Controllers
             _configuration = configuration;
             _webHostEnvironment = webHostEnvironment;
         }
+        [HttpGet("{id}")]
+        public JsonResult Get(int id)
+        {
+            string query = @"
+                SELECT FirstName, MiddleName, LastName, AboutMe, OwnerRole,PhoneNumber,EmailURL,LinkedInURL,GitHub
+                FROM dbo.PortfolioOwner
+                WHERE OwnerId = @OwnerId";
+            DataTable _table = new DataTable();
+            // Retrieve the connection string from the configuration
+            string _sqlDataSource = _configuration.GetConnectionString("PortfolioCon");
+            SqlDataReader _myReader;
+            using (SqlConnection _myCon = new SqlConnection(_sqlDataSource))
+            {
+                // Open the connection
+                _myCon.Open();
+                // Create a SqlCommand to execute the query
+                using (SqlCommand _myCommand = new SqlCommand(query, _myCon))
+                {
+                    // Add parameters to the command
+                    _myCommand.Parameters.AddWithValue("@OwnerId", id);
+                    _myReader = _myCommand.ExecuteReader();
+                    _table.Load(_myReader);
+                    _myReader.Close();
+                    _myCon.Close();
+                }
+            }
+            return new JsonResult(_table);
+        }
 
         [HttpGet]
         public JsonResult Get()
@@ -87,7 +115,7 @@ namespace PortfolioAPI.Controllers
         public JsonResult Put(PortfolioOwner _owner)
         {
             string query = @"
-                UPDATE dbo.PortfiolioOwner
+                UPDATE dbo.PortfolioOwner
                 SET FirstName = @FirstName, MiddleName = @MiddleName, LastName = @LastName, AboutMe = @AboutMe, OwnerRole = @OwnerRole,PhoneNumber = @PhoneNumber,EmailURL = @EmailURL,LinkedInURL = @LinkedInURL,GitHub = @GitHub
                 WHERE OwnerId = @OwnerId";
             DataTable _table = new DataTable();
@@ -111,6 +139,7 @@ namespace PortfolioAPI.Controllers
                     _myCommand.Parameters.AddWithValue("@EmailURL", _owner.EmailURL);
                     _myCommand.Parameters.AddWithValue("@LinkedInURL", _owner.LinkedInURL);
                     _myCommand.Parameters.AddWithValue("@GitHub", _owner.GitHub);
+                    _myCommand.Parameters.AddWithValue("@OwnerId", _owner.OwnerId);
                     // Execute the command
                     _myReader = _myCommand.ExecuteReader();
                     _table.Load(_myReader);
@@ -148,26 +177,26 @@ namespace PortfolioAPI.Controllers
             }
             return new JsonResult("Deleted Successfully");
         }
-        [Route("Save File")]
-        [HttpPost]
-        public JsonResult SaveFile(IFormFile file)
-        {
-            try
-            {
-               var httpRequest = Request.Form;
-                var postedFile = httpRequest.Files[0];
-                string fileName = postedFile.FileName;
-                var physicalPath = _webHostEnvironment.ContentRootPath + "/Images/" + fileName;
-                using (var stream = new FileStream(physicalPath, FileMode.Create))
-                {
-                    postedFile.CopyTo(stream);
-                }
-                return new JsonResult(fileName);
-            }
-            catch (Exception ex)
-            {
-                return new JsonResult($"Error: {ex.Message}");
-            }
-        }
+        //[Route("Save File")]
+        //[HttpPost]
+        //public JsonResult SaveFile(IFormFile file)
+        //{
+        //    try
+        //    {
+        //       var httpRequest = Request.Form;
+        //        var postedFile = httpRequest.Files[0];
+        //        string fileName = postedFile.FileName;
+        //        var physicalPath = _webHostEnvironment.ContentRootPath + "/Images/" + fileName;
+        //        using (var stream = new FileStream(physicalPath, FileMode.Create))
+        //        {
+        //            postedFile.CopyTo(stream);
+        //        }
+        //        return new JsonResult(fileName);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return new JsonResult($"Error: {ex.Message}");
+        //    }
+        //}
     }
 }
